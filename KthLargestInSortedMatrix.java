92bbce1b217edff7f1ffa7ad1a25b70cc8b5c8de
Alain Brown <alain.brown@gmail.com>
2015-11-13 23:01:28 -0800

    Update KthLargestInSortedMatrix.java

--- KthLargestInSortedMatrix.java
+++ KthLargestInSortedMatrix.java
@@ -6,33 +6,33 @@ import java.util.PriorityQueue;
 
 public class KthLargestInSortedMatrix {
 	
-	private static class V implements Comparable<V> {
-		int val,row,col;
-		private V(int val,int row,int column){
+	private static class V<E extends Comparable<E>> implements Comparable<V<E>> {
+		E val; int row,col;
+		private V(E val,int row,int column){
 			this.val=val;
 			this.row=row;
 			this.col=column;
 		}
-		public int compareTo(V o) { return this.val - o.val; }
+		public int compareTo(V<E> o) { return this.val.compareTo(o.val); }
 	}
 
-	// Complexity: O(k*log(n)) n rows, kth largest number
+	// Complexity: O(k*log(n)) n rows, kth largest
 	// Space: O(n)
-	public static int largest(int k, int[][]matrix){
-		PriorityQueue<V> h = new PriorityQueue<>();
+	public static <E extends Comparable<E>> E largest(int k, E[][]matrix) {
+		PriorityQueue<V<E>> h = new PriorityQueue<>();
 		for (int i=0;i<matrix.length;i++) 
-			if (matrix[i].length>0) h.offer(new V(matrix[i][0],i,0));
+			if (matrix[i].length>0) h.offer(new V<E>(matrix[i][0],i,0));
 		for (int i=1; i<k && !h.isEmpty(); i++){
-			V v = h.poll();
+			V<E> v = h.poll();
 			if (v.col+1 < matrix[v.row].length) 
-				h.offer(new V(matrix[v.row][v.col+1],v.row,v.col+1));
+				h.offer(new V<E>(matrix[v.row][v.col+1],v.row,v.col+1));
 		}
 		if (h.isEmpty()) throw new IllegalArgumentException();
 		return h.peek().val;
 	}
 	
-	public static void main(String[] args){
-		int[][] M = {
+	public static void main(String[] args) {
+		Integer[][] M = {
 				{2,4,6,8,10},
 				{12,20,50,60},
 				{1,3,5,7,9,11},

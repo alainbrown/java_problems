c7983e18f5c260452f141a5162cb949459cae075
Alain Brown <alain.brown@gmail.com>
2015-11-18 01:34:05 -0800

    Create MergeSortedArrays.java

---
+++ MergeSortedArrays.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.Writer;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+//Given an active stream of sorted arrays, how would you merge them efficiently?
+
+public class MergeSortedArrays {
+	
+	static class N implements Comparable<N> {
+		String val;
+		int arr;
+		N(String val, int arr) {
+			this.val=val;
+			this.arr=arr;
+		}
+		@Override
+		public int compareTo(N o) { return val.compareTo(o.val);}
+	}
+	
+	// Complexity: n*m log(n), for n arrays of length m
+	// Space: O(n)
+	public void merge(Scanner[] in, Writer out) throws IOException {
+		PriorityQueue<N> h = new PriorityQueue<>();
+		for (int i=0; i< in.length; i++)
+			if (in[i].hasNext()) h.offer(new N(in[i].next(),i));
+		while (!h.isEmpty()) {
+			N n = h.poll();
+			out.write(n.val);
+			if (in[n.arr].hasNext()) h.offer(new N(in[n.arr].next(),n.arr));
+		}
+	}
+}

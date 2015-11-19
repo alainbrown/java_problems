16f40fa48a0605cc33cde858219ffaa99027cbf3
Alain Brown <alain.brown@gmail.com>
2015-11-18 18:45:41 -0800

    Update MinDigitSum.java

--- MinDigitSum.java
+++ MinDigitSum.java
@@ -1,28 +1,22 @@
-package java_problems;
-
 import java.util.Arrays;
 
 public class MinDigitSum {
-	
-	// Complexity: O(n logn)
+
+	// Complexity: O(n log n)
 	// Space: O(n)
-	public static int minSum(int[] arr){
+	public static int minSum(int[] arr) {
 		Arrays.sort(arr);
 		int leftSize = arr.length/2 + arr.length % 2;
 		int rightSize = arr.length/2;
-		boolean check = true;
-		int l=0;
-		int r=0;
+		int sum = 0;
 		for (int i=0;i<arr.length; i++) {
-			if (check) l+= Math.pow(10, --leftSize)*arr[i];
-			else r+= Math.pow(10, --rightSize)*arr[i];
-			check=!check;
+			if (i%2==0) sum+= Math.pow(10, --leftSize)*arr[i];
+			else sum+= Math.pow(10, --rightSize)*arr[i];
 		}
-		return l+r;
+		return sum;
 	}
 
 	public static void main(String[] args) {
 		System.out.println(minSum(new int[]{2,1,5,3,7,9,11,4}));
 	}
-
 }

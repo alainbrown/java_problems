68263ce178181f2972945de2dbf72b6253a8013c
Alain Brown <alain.brown@gmail.com>
2015-11-21 17:51:00 -0800

    Update ScoreOccurrence.java

--- ScoreOccurrence.java
+++ ScoreOccurrence.java
@@ -9,45 +9,53 @@ public class ScoreOccurrence {
 	// Complexity: O(log n)
 	// Space: O(1)
 	public static int occurrence(int[] arr, int score) {
-		int ans= findLast(arr, score) - findFirst(arr, score);
-		return ans < 0 ? 0 : ans;
+		int last = lastIndex(arr, score);
+		int first = firstIndex(arr, score);
+		if (last>=0 && last==first) return 1;
+		else if (last<0) return 0;
+		else return last-first;
 	}
 
-	static int findFirst(int[] arr, int score) {
+	static int firstIndex(int[] arr, int score) {
 		int bottom = 0;
 		int top = arr.length - 1;
 		int mid = -1;
-
+		boolean found = false;
+		
 		while (top >= bottom) {
 			mid = (top + bottom) / 2;
+			if (arr[mid]==score) found = true;
 			if (arr[mid] >= score) top = mid - 1;
 			else if (arr[mid] < score) {
 				bottom = mid + 1;
 				if (arr[mid]==score) return mid;
 			}
 		}
-		return mid;
+		return found ? mid : -1;
 	}
 
-	static int findLast(int[] arr, int score) {
+	static int lastIndex(int[] arr, int score) {
 		int bottom = 0;
 		int top = arr.length - 1;
 		int mid = -1;
-
+		boolean found = false;
+		
 		while (top >= bottom) {
 			mid = (top + bottom) / 2;
+			if (arr[mid]==score) found = true;
 			if (arr[mid] <= score) bottom = mid + 1;
 			else if (arr[mid] > score) {
 				top = mid - 1;
 				if (arr[mid]==score) return mid;
 			}
 		}
-		return mid;
+		return found ? mid : -1;
 	}
 
 	public static void main(String[] args) {
 		System.out.println(occurrence(new int[]{0,1,10,10,10,10,20,30},7));
 		System.out.println(occurrence(new int[]{0,1,10,10,10,10,20,30},10));
+		System.out.println(occurrence(new int[]{0,1,10,10,10,10,20,30},30));
 	}
 
 }
